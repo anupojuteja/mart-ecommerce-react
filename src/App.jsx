df2d4573fa0a003872bcd0e617efa9352166877b
anupojuteja <anupojunagamaniteja@gmail.com>
2025-05-30 17:11:51 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Card/>
     </>
   )
 }
