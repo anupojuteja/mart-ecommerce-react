2e52d665f1ceb70d895c273f5662c39822a985f1
anupojuteja <anupojunagamaniteja@gmail.com>
2025-06-07 17:41:53 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './Components/Header'
-import Card from './Components/Card'
+import  Header from './Components/Header'
+// import { Homepage } from './Componets/Homepage'
+// import { Shop } from './Componets/Shop'
+// import { Cart } from './Componets/Cart'
+// import { Footer } from './Componets/Footer'
+// import { Productdetails } from './Componets/ProductDetails'
+// import { CartContext } from './Componets/CartContext'
+import { useState } from 'react'
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart,setCart]=useState([])
+  const handleAddToCartToasfy = () => {
+  toast.success('Product has been added to cart!');
+};
 
   return (
     <>
-    <Header/>
-    <Card/>
+    {/* <CartContext.Provider value={[cart,setCart]}> */}
+    <Header cout={cart.length}/>
+    {/* <ToastContainer position="top-right" autoClose={3000} /> */}
+    <Routes>
+      <Route path='/' element={<Homepage toasfy={handleAddToCartToasfy()}/> } />
+      {/* <Route path='/home' element={<Homepage toasfy={handleAddToCartToasfy()}/> }/>
+      <Route path='/shop' element={<Shop toasfy={handleAddToCartToasfy()}/>} />
+      <Route path='/cart' element={<Cart toasfy={handleAddToCartToasfy()}/>}/>
+      <Route path='/:id' element={<Productdetails toasfy={handleAddToCartToasfy()}/>}/>
+    </Routes> */}
+    <Footer/>
+    {/* </CartContext.Provider> */}
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,60 +1,28 @@
-import { Link } from 'react-router-dom'
-import logo from 'src\assets\achieversIT.png'
-//import '../Componets/Header.css'
-export const Header = ({cout})=>{
-    return(
-        <>
-        <div className='headerContainer'>
-            <div className='headerImage'>
-                <img src={logo} alt="Logo" />
-            </div>
-            <div>
-                <nav>
-                    <ul className='headerLinks'>
-                        <Link to={'/home'}><li>Home</li></Link>
-                        <Link to={'/shop'}><li>Shop</li></Link>
-                        <Link to={'/cart'}><li>Cart<sup>{cout}</sup></li></Link>
-                        <li><i className="fa-solid fa-user"></i></li>
-                        <Link to={'/cart'}><li><i className="fa-solid fa-cart-shopping"></i></li></Link>
-                    </ul>
-                </nav>
-            </div>
+import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../assets/achieversIT.png'
+
+
+const Header = ({count}) => {
+  return (
+    <>
+    <div className='HeaderContainer'>
+        <div className='HeaderLogo'>
+            <img src={logo} alt="logo" />
         </div>
-        </>
-    )
+        <div className=' HeaderTitle'>
+            <nav> 
+                <ul className='Cards'>
+                    <Link to={'/home'}><li>Home</li></Link>
+                    <p>thebjdbj</p>
+                    
+                </ul>
+            </nav>
+        </div>
+    </div>
+    </>
+
+  )
 }
 
-
-
-
-
-// import React,{useState} from 'react'
-
-// const Header = () => {
-//     const [count,setCount] = useState(0)
-//     const [name,setName] = useState('')
-//     const [isLogged, setIsLogged] = useState(true)
-//     const [color, setcolor]= useState()
-//     console.log(count)
-//   return (
-//     <div>
-
-//         <p>{count}</p>
-//         <button onClick={()=>setCount(count+4)}>increment</button>
-//         <button onClick={()=>setCount(count-4)}>decrement</button>
-//         <h3><input type="text" onChange={(e)=>setName(e.target.value)} /></h3>
-//         <h1>{name}</h1>
-//         <input type={isLogged?'password':'text'}/>
-//         <button onClick={()=>setIsLogged(!isLogged)}>show</button>
-//         <p style={{backgroundColor:color}}>this is color changing para</p>
-//         <button onClick={()=>setcolor('red')}>RED</button>
-//         <button onClick={()=>setcolor('pink')}>pink</button>
-//         <button onClick={()=>setcolor('green')}>green</button>
-
-
-      
-//     </div>
-//   )
-// }
-
-// export default Header
+export default Header
