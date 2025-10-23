899c4c88756f9473036f17e441a0ef981a957710
anupojuteja <anupojunagamaniteja@gmail.com>
2025-10-24 01:43:45 +0530

    Rectifying the errors

--- src/Components/BigDiscount.jsx
+++ src/Components/BigDiscount.jsx
@@ -2,17 +2,23 @@ import { discoutProducts } from "../assets/Products.js"
 import { ProductsCard } from "./ProductCards"
 import '../Components/BigDiscount.css'
 
-export const BigDiscount = ()=>{
-    return(
+export const BigDiscount = () => {
+    return (
         <>
-        <div><h1 className="Name">Big Discount</h1></div>
-        <div className="productsContainer">
-            {discoutProducts.map((item)=>(
-                <div className="productswidth">
-                    <ProductsCard key={item.id} imgUrl={item.imgUrl} productName={item.productName} id={item.id} price={item.price} discount={item.discount} />
-                </div>
-            ))}
-        </div>
+            <div><h1 className="Name">Big Discount</h1></div>
+            <div className="productsContainer">
+                {discoutProducts.map((item) => (
+                    <div className="productswidth" key={item.id}>
+                        <ProductsCard
+                            imgUrl={item.imgUrl}
+                            productName={item.productName}
+                            id={item.id}
+                            price={item.price}
+                            discount={item.discount}
+                        />
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
--- src/Components/ProductCards.jsx
+++ src/Components/ProductCards.jsx
@@ -3,17 +3,25 @@ import './ProductCards.css'
 import { useContext } from 'react';
 import { CartContext } from './CartContext';
 import { toast } from 'react-toastify';
+
 export const ProductsCard = (props) => {
-    const [cart,setCart]=useContext(CartContext)
-    const handleAddToCartToasfy = () => {
-        toast.success('Product has been added to cart!');
+    // Object destructuring for context
+    const { cart, setCart, handleAddToCartToasfy } = useContext(CartContext);
+
+    const handleExistingProduct = () => {
+        toast.info('Product quantity increased!');
     };
+
     return (
         <div className="productCard">
-            <Link to={`/${props.id}`}><div className='productCardImage'>
-                <img src={props.imgUrl} alt={props.productName} />
-                <div className='poductCardLikeIcon'><i class="fa-regular fa-heart"></i></div>
-            </div></Link>
+            <Link to={`/${props.id}`}>
+                <div className='productCardImage'>
+                    <img src={props.imgUrl} alt={props.productName} />
+                    <div className='poductCardLikeIcon'>
+                        <i className="fa-regular fa-heart"></i>
+                    </div>
+                </div>
+            </Link>
             <div className='productDetails'>
                 <h2>{props.productName}</h2>
                 <span>
@@ -26,20 +34,21 @@ export const ProductsCard = (props) => {
                 <h3>${props.price}</h3>
                 <div className='productDetailsButton'>
                     <button
-                    onClick={()=>{
-                        const existingProductIndex=cart.findIndex(p=>p.id===props.id);
-
-                        if(existingProductIndex !==-1){
-                            const updatedCart = [...cart];
-                            updatedCart[existingProductIndex].quantity=(updatedCart[existingProductIndex].quantity||1)+1;
-                            setCart(updatedCart);
-                            handleExistingProdct()
-                        }else{
-                            setCart([...cart,{...props,quantity:1}]);
-                            handleAddToCartToasfy()
-                        }
-                    }}
-                    ><i className="fa-solid fa-plus"></i></button>
+                        onClick={() => {
+                            const existingProductIndex = cart.findIndex(p => p.id === props.id);
+                            if (existingProductIndex !== -1) {
+                                const updatedCart = [...cart];
+                                updatedCart[existingProductIndex].quantity = (updatedCart[existingProductIndex].quantity || 1) + 1;
+                                setCart(updatedCart);
+                                handleExistingProduct();
+                            } else {
+                                setCart([...cart, { ...props, quantity: 1 }]);
+                                handleAddToCartToasfy();
+                            }
+                        }}
+                    >
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
                 </div>
             </div>
             <div className='productDiscount'><p>{props.discount}% Off</p></div>
