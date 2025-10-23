afbed81556906c0677b759312dd933b4e9558c19
anupojuteja <anupojunagamaniteja@gmail.com>
2025-10-23 23:51:26 +0530

    bestsales

--- src/Components/BestSales.jsx
+++ src/Components/BestSales.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import '../Componets/BestSales.css'
+import { sofa } from "../assets/filterArray"
+import { ProductsCardNewBest } from "./ProductsCardNewBest"
 
-const BestSales = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export const BestSales = ({toasfy})=>{
+    return(
+        <>
+        <div><h1 className="NameBestSales">Best Sales</h1></div>
+        <div className="productsContainerBestSales">
+            {sofa.map((item)=>(
+                <div>
+                    <ProductsCardNewBest key={item.id} imgUrl={item.imgUrl} productName={item.productName} id={item.id} price={item.price}/>
+                </div>
+            ))}
+        </div>
+        </>
+    )
 }
-
-export default BestSales
